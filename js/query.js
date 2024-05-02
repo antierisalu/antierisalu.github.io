e3fdbe78e92c2572cb97d7b32bd3ad64fdc46c5b
antierisalu <antikaz@gmail.com>
2024-05-02 12:57:24 +0300

    i have data

--- js/query.js
+++ js/query.js
@@ -10,11 +10,25 @@ async function getUserData() {
         },
         body: JSON.stringify({
           query: `{
-          user {
-            auditRatio
-            attrs
-          }
-        }`,
+            user{
+              auditRatio
+              login
+              attrs
+              xps (where: { event: { id: { _eq: 148  } } } ){
+                path
+                amount
+                event {
+                  id
+                }
+              }
+            }
+            
+            result (where: {type: {_eq: "user_audit"}}) {
+                      grade
+                      path
+
+                    }
+          }`,
         }),
       }
     );
@@ -26,8 +40,34 @@ async function getUserData() {
 
     const data = await response.json();
     console.log(data);
-    const auditRatio = data.data.user[0].auditRatio;
+    
+
+    const auditRatio = data.data.user[0].auditRatio.toFixed(2)
     const attrs = data.data.user[0].attrs;
+    const login = data.data.user[0].login
+
+    const grades = data.data.result
+    const gradeData = []
+
+    const xp = data.data.user[0].xps
+    const xpData = [];
+    let totalXpAmount = 0;
+
+    xp.forEach(item => {
+      xpData.push({
+        path: item.path,
+        amount: (item.amount / 1000).toFixed(0)
+      });
+      totalXpAmount += item.amount / 1000
+    });
+
+    grades.forEach(item => {
+      gradeData.push({
+        path: item.path,
+        grade: item.grade.toFixed(2)
+      })
+
+    })
 
     let {
       tel,
@@ -40,20 +80,20 @@ async function getUserData() {
       personalIdentificationCode,
     } = attrs;
 
-    populateProfile(firstName, lastName, email, tel, personalIdentificationCode, addressStreet, addressCity, addressCountry)
+    displayProfile(firstName, lastName, email, tel, personalIdentificationCode, addressStreet, addressCity, addressCountry)
+    displayXps(xpData, totalXpAmount)
+    displayGrades(gradeData, auditRatio)
 
-    console.log("Audit Ratio:", auditRatio);
+    document.getElementById("topBarUserName").innerHTML = login
+
   } catch (error) {
     console.error("Error fetching user data:", error.message);
   }
 }
 
-function populateProfile(firstName, lastName, email, tel, personalIdentificationCode, addressStreet, addressCity, addressCountry) {
-  // Get the element with id "profile"
-  let profileElement = document.getElementById("profile");
+function displayProfile(firstName, lastName, email, tel, personalIdentificationCode, addressStreet, addressCity, addressCountry) {
 
-  // Update the inner HTML of the profile element with the dynamic data
-  profileElement.innerHTML = `
+  document.getElementById("profile").innerHTML = `
   <div class="boxData">${firstName} ${lastName}</div>
   <div class="boxData">${email}</div>
   <div class="boxData">${tel}</div>
@@ -62,3 +102,48 @@ function populateProfile(firstName, lastName, email, tel, personalIdentification
 `;
 }
 
+function displayGrades(gradeData, auditRatio) {
+
+  document.getElementById("auditRatio").innerHTML = `<div class="boxData">Audit Ratio: ${auditRatio}</div>`;
+
+  const dataContainer = document.getElementById('grades')
+  dataContainer.innerHTML = ''
+
+  gradeData.forEach(item => {
+    const itemDiv = document.createElement('div')
+    
+    const pathElement = document.createElement('p')
+    pathElement.textContent = `Task: ${item.path}`
+
+    const gradeElement = document.createElement('p')
+    gradeElement.textContent = `Grade: ${item.grade}`
+
+    itemDiv.appendChild(pathElement)
+    itemDiv.appendChild(gradeElement)
+
+    dataContainer.appendChild(itemDiv)
+  })
+}
+
+function displayXps(xpData, totalXpAmount) {
+
+  document.getElementById("totalXp").innerHTML = `<div class="boxData">Total Experience Points: ${totalXpAmount} Kb</div>`;
+  
+  const dataContainer = document.getElementById('xp');
+  dataContainer.innerHTML = '';
+
+  xpData.forEach(item => {
+    const itemDiv = document.createElement('div');
+
+    const pathElement = document.createElement('p');
+    pathElement.textContent = `Task: ${item.path}`;
+
+    const amountElement = document.createElement('p');
+    amountElement.textContent = `Experience Points: ${item.amount} Kb`;
+
+    itemDiv.appendChild(pathElement);
+    itemDiv.appendChild(amountElement);
+
+    dataContainer.appendChild(itemDiv);
+  });
+}
